14b2b3642dcd74cb581ca880098de9be7ea397c9
Brian Clovis <bwclovis@gmail.com>
2014-12-15 11:00:43 -0500

    concat javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp'),
 		gutil = require('gulp-util'),
-		coffee = require('gulp-coffee');
+		coffee = require('gulp-coffee'),
+		concat = require('gulp-concat');
 
-var coffeeSrc = ['components/coffee/tagline.coffee']
+var coffeeSrc = ['components/coffee/tagline.coffee'];
+var jsSource = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js'
+	];
 
 gulp.task('coffee',function(){
 	gulp.src(coffeeSrc)
 		.pipe(coffee({bare: true})
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+	gulp.src(jsSource)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
